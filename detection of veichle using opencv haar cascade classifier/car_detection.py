f59531a9dd3e1d6fa47dc327c4f9c95905d48f0e
anozele <anoze360@gmail.com>
2019-01-08 03:26:26 -0800

    Update car_detection.py

--- detection of veichle using opencv haar cascade classifier/car_detection.py
+++ detection of veichle using opencv haar cascade classifier/car_detection.py
@@ -19,6 +19,27 @@ while True:
     for (x,y,w,h) in cars:
         p=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
     
+     if w > 50 and h > 50:
+                    cv2.rectangle(frame, (x,y), (x+w,y+h), (255, 100, 30), 2)
+
+                    x1 = w/2      
+                    y1 = h/2
+
+                    cx = x
+                    cy = y
+                    centroid = (cx,cy)
+                    cv2.circle(frame,(int(cx),int(cy)),2,(0,1,255),3)
+
+                    if centroid > (43, 10) and centroid < (50, 10):
+                        
+                        counter +=1
+                        print ("counter=", counter)
+
+                    cv2.putText(frame, str(counter), (x,y-5),
+                                        cv2.FONT_HERSHEY_SIMPLEX,
+                                        0.5, (0, 0, 255), 2)
+            
+    
 
     cv2.imshow('traffic countt', frame)
     if cv2.waitKey(25) & 0xFF == ord('q'):
